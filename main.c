3192d9a65731f24a9fa7d16c1498399881924a96
sys7375 <juliobenin@yahoo.com.br>
2022-12-15 16:33:02 -0300

    retornado para o primeiro commit, readicionado make, criado um cabeçalho

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 int main(void)
 {
     char cmd;
 
     system("/bin/stty raw");
+    system("clear");
 
-    while((int)(cmd = getchar()) != 27);
+    while((int)(cmd = getchar()) != 27)
+    {
+        system("clear");
+        printf("%i",cmd);
+    }
 
     system("/bin/stty cooked");
+    system("clear");
 
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef __RPGNatal__
+#define __RPGNatal__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
